55d43ccd6f966a726175f6cb28a5bdc3697d854d
Natanæl Elver <river0@hackermail.com>
2022-12-18 05:01:29 +0200

    modified russianroulette.py according to pycharm's suggestions

--- py/russianroulette/russianroulette.py
+++ py/russianroulette/russianroulette.py
@@ -4,24 +4,27 @@ from time import sleep
 
 path = input("where do you want to risk?\nplease use forward slashes (/) and not backslashes (\\).\n> ")
 
-def fileSet():
-    pathlist = os.listdir(path)
-    if len(pathlist) == 0:
+
+def file_set():
+    _path_list = os.listdir(path)
+    if len(_path_list) == 0:
         print("no files in the specified directory. exiting...")
         exit()
-    file = random.choice(pathlist)
-    return file
+    set_file = random.choice(_path_list)
+    return set_file
+
 
 a = True
 while a:
     try:
-        pathlist = os.listdir(path)
+        path_list = os.listdir(path)
         a = False
     except OSError:
-        path = input(f"{path} doesn't exist.\nplease choose a valid path\n(make sure it is the whole path (including C:\ for windows))\n> ")
+        path = input(f"{path} doesn't exist.\nplease choose a valid path\n(make sure it is the whole path "
+                     f"(including C:\\ for windows))\n> ")
 
-print(pathlist)
-fileSet()
+print(path_list)  # noqa
+file_set()
 
 shoot = input("press enter to shoot")
 if len(shoot) != 0:
@@ -35,14 +38,12 @@ else:
     b = True
 
 while b:
-    file = fileSet()
+    file = file_set()
     if random.randint(0, 6) == 2:
         print(f"unlucky...deleting {file}")
-        os.remove(f"{path}\{file}")
+        os.remove(f"{path}\\{file}")
     else:
         print("safe")
     shoot = input("shoot again? (press enter to shoot)")
     if len(shoot) != 0:
         b = False
-
-# needs to be checked for bugs, can you help :D
