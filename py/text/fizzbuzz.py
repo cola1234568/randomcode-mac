51c1d72b81891e5c00a73447b234aacbdef7b0fa
Natanæl Elver <river0@hackermail.com>
2022-12-19 16:18:23 -0500

    added /py/text/ with new fizzbuzz.py in it

---
+++ py/text/fizzbuzz.py
@@ -0,0 +1,10 @@
+for i in range(100):
+    new_str = ""
+    if i % 3 == 0 or i % 5 == 0:
+        if i % 3 == 0:
+            new_str += 'Fizz'
+        if i % 5 == 0:
+            new_str += 'Buzz'
+        print(new_str)
+    else:
+        print(i)
